0d9e9403df659c2e316eca2e95ba26207751ceed
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-12-11 15:10:38 +0100

    contacts slice

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,34 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const contactsInitialState = [
+  { name: 'mama', number: '123456789', id: nanoid() },
+  { name: 'tata', number: '000000000', id: nanoid() },
+];
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: contactsInitialState,
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(name, number) {
+        return {
+          payload: {
+            name,
+            number,
+            id: nanoid(),
+          },
+        };
+      },
+    },
+    deleteContact(state, action) {
+      const index = state.findIndex(contact => contact.id === action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
